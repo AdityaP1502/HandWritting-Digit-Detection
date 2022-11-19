89630f085051b50ff9908c3f3e7d01bb3203e4db
AdityaP1502 <i.m.aditya015@gmail.com>
2022-11-19 20:26:44 +0700

    outline: Update loop counter algorithm

--- src/main/outline.py
+++ src/main/outline.py
@@ -2,447 +2,156 @@ import numpy as np
 from src.main.graph import Vertex
 from os.path import abspath
 from ctypes import *
-# class Position():
-#   def __init__(self, i, j) -> None:
-#     self.i = i
-#     self.j = j
-  
-#   def __eq__(self, __o: object) -> bool:
-#     if __o.i == None:
-#       return __o.j == self.j
-    
-#     if __o.j == None:
-#       return __o.i == self.i
-    
-#     return (self.i == __o.i and self.j == __o.j)
-  
-#   def __repr__(self) -> str:
-#     return '({}, {})'.format(self.i, self.j)
-
-# class Outline():
-#   def __init__(self, img) -> None:
-#     ny, nx = np.shape(img)
-#     # self.img = np.full((ny+1, nx + 1), 255)
-
-#     # for i in range(1, ny + 1):
-#     #   for j in range(1, nx + 1):
-#     #     self.img[i][j] = img[i - 1][j - 1]
-#     # print(self.img)
-#     self.img = img
-#     self.ny = ny
-#     self.nx = nx
-    
-#     self.update_pos = [
-#       self.__updateFnc_1, 
-#       self.__updateFnc_2, 
-#       self.__updateFnc_3, 
-#       self.__updateFnc_4, 
-#     ]
-
-#     self.end_pos = [
-#       Position(None, -1), 
-#       Position(-1, None), 
-#       Position(None, nx),
-#       Position(ny, None), 
-#     ]
-    
-#     self.dp = [[None for i in range(self.nx)] for j in range(self.ny)]
-  
-#   @staticmethod
-#   def __updateFnc_1(pos):
-#     pos.j -= 1
-  
-#   @staticmethod
-#   def __updateFnc_2(pos):
-#     pos.i -= 1
-
-#   @staticmethod
-#   def __updateFnc_3(pos):
-#     pos.j += 1
-
-#   @staticmethod
-#   def __updateFnc_4(pos):
-#     pos.i += 1
-    
-#   def isBoundary(self, pos):
-#     ds = [-1, 0, 1]
-#     if pos.i < 0 or pos.i >= self.ny: return False
-#     if pos.j < 0 or pos.j >= self.nx: return False
-#     if self.img[pos.i][pos.j] == 255: return False
-    
-#     for dy_ in ds:
-#       for dx_ in ds:
-#         i = pos.i + dy_
-#         j = pos.j + dx_
-
-#         if i < 0 or i >= self.ny: return True
-#         if j < 0 or j >= self.nx: return True
-
-#         if self.img[i][j] == 255:
-#           return True
-      
-#     return False
-
-#   def getNeighborBoundary(self, pos):
-#     ds = [-1, 0, 1, 0]
-#     loc = []
-#     curr_loc = Position(None, None)
-#     for k in range(4):
-#       dy = ds[(k + 3) % 4]
-#       dx = ds[k]
-      
-#       curr_loc.i = pos.i + dy
-#       curr_loc.j = pos.j + dx
-      
-#       if curr_loc.i < 0 or curr_loc.i >= self.ny: continue
-#       if curr_loc.j < 0 or curr_loc.j >= self.nx: continue
-      
-#       if self.isBoundary(curr_loc):
-#         if self.dp[curr_loc.i][curr_loc.j] != None: continue
-#         loc.append(k)
-        
-#     return loc
-  
-#   def tranverse(self, stack):
-#     while len(stack) > 0:
-#       root_pos, root_vertex, direction = stack.pop()
-#       curr_loc = Position(root_pos.i, root_pos.j)
-#       self.update_pos[direction](curr_loc)
-#       exist = False
-
-#       while (curr_loc != self.end_pos[direction] and self.isBoundary(curr_loc)):
-#         dirs = self.getNeighborBoundary(curr_loc)
-
-#         if self.dp[curr_loc.i][curr_loc.j] != None:
-#           root_vertex.add_connection(self.dp[curr_loc.i][curr_loc.j], direction)
-#           self.update_pos[direction](curr_loc)
-#           continue
-
-#         for direction_ in dirs:
-#           if direction_ != direction:
-#             exist = True
-#             break
-        
-#         if exist:
-#           new_loc = Position(curr_loc.i, curr_loc.j)
-
-#           new_vertex = Vertex(new_loc)
-#           root_vertex.add_connection(new_vertex, direction)
-#           root_vertex = new_vertex
-
-#           for direction_ in dirs:
-#             if direction_ != direction:
-#               stack.append([new_loc, new_vertex, direction_])
-
-#           exist = False
-
-#         self.dp[curr_loc.i][curr_loc.j] = root_vertex
-#         self.update_pos[direction](curr_loc)
-    
-#   def __fill(self, img, loc, end_loc, direction):
-#     ds = [-1, 0, 1, 0]
-#     curr_loc = Position(loc.i, loc.j)
-#     while curr_loc != end_loc:
-#       for k in range(4):
-#         i = curr_loc.i + ds[(3 + k) % 4]
-#         j = curr_loc.j + ds[k]
-
-#         if i < 0 or i >= self.ny: continue
-#         if j < 0 or j >= self.nx: continue
-
-#         if img[i][j] == 255:
-#           img[i][j] = 127 
-          
-#       self.update_pos[direction](curr_loc)
-        
-#   def findOutline(self):
-#     outlines = []
-#     stack = []
-#     for i in range(self.ny):
-#       for j in range(self.nx):
-#         if self.dp[i][j] == None:
-#           start_pos = Position(i, j)
-#           if not self.isBoundary(start_pos): continue
-#           start_vertex = Vertex(start_pos)
-#           self.dp[start_pos.i][start_pos.j] = start_vertex
-#           dirs = self.getNeighborBoundary(start_pos)
-
-#           # tranvrse new node
-#           for direction in dirs:
-#             stack.append([start_pos, start_vertex, direction])
-
-#           self.tranverse(stack)
-            
-#           outlines.append(start_vertex)
-          
-#     return outlines
-  
-  
-#   def makeOutline(self, outlines, result = [], dict = None):
-#     if len(result) == 0:
-#       result = np.copy(self.img)
-      
-#     if dict == None:
-#       dict = {}
-
-#     for outline in outlines:
-#       if dict.get((outline.key.i, outline.key.j), False) == True: continue
-#       dict[(outline.key.i, outline.key.j)] = True
-      
-#       for conn in outline.conn:
-#         direction = Vertex.connectionDirection(outline.key, conn.key)
-#         for k in range(4):
-#           if direction[k] == 1:
-#             self.__fill(result, outline.key, conn.key, k)
-            
-#       result = self.makeOutline(outline.conn, result)
-    
-#     return result
-class Position():
-  def __init__(self, i, j) -> None:
-    self.i = i
-    self.j = j
-  
-  def __eq__(self, __o: object) -> bool:
-    if __o.i == None:
-      return __o.j == self.j
-    
-    if __o.j == None:
-      return __o.i == self.i
-    
-    return (self.i == __o.i and self.j == __o.j)
-  
-  def __repr__(self) -> str:
-    return '({}, {})'.format(self.i, self.j)
 
 class Outline():
-  def __init__(self, img) -> None:
-    ny, nx = np.shape(img)
-    # self.img = np.full((ny+1, nx + 1), 255)
+    __BG_PIXELS_INTENSITY = 0
 
-    # for i in range(1, ny + 1):
-    #   for j in range(1, nx + 1):
-    #     self.img[i][j] = img[i - 1][j - 1]
-    # print(self.img)
-    self.img = img
-    self.ny = ny
-    self.nx = nx
-    self.isUnique = True
-    self.uniqueVertexCount = 0
-    
-    self.update_pos = [
-      self.__updateFnc_1, 
-      self.__updateFnc_2, 
-      self.__updateFnc_3, 
-      self.__updateFnc_4, 
-    ]
+    def __init__(self, img) -> None:
+        ny, nx = np.shape(img)
+        self.img = img
+        self.ny = ny
+        self.nx = nx
+        self.__dp = [[0 for j in range(nx)] for i in range(ny)]
 
-    self.end_pos = [
-      Position(None, -1), 
-      Position(-1, None), 
-      Position(None, nx),
-      Position(ny, None), 
-    ]
-    
-    self.dp = [[0 for i in range(self.nx)] for j in range(self.ny)]
-  
-  @staticmethod
-  def __updateFnc_1(pos):
-    pos.j -= 1
-  
-  @staticmethod
-  def __updateFnc_2(pos):
-    pos.i -= 1
+    def __checkOtherRegion(self, i, j):
+        return (self.__dp[i][j] != 0)
 
-  @staticmethod
-  def __updateFnc_3(pos):
-    pos.j += 1
-
-  @staticmethod
-  def __updateFnc_4(pos):
-    pos.i += 1
-    
-  def boundaryVector(self, pos):
-    ds = [-1, 0, 1]
-    vec = []
-    if pos.i < 0 or pos.i >= self.ny: return vec
-    if pos.j < 0 or pos.j >= self.nx: return vec
-    if self.img[pos.i][pos.j] == 0: return vec
-    
-    for dy_ in ds:
-      for dx_ in ds:
-        i = pos.i + dy_
-        j = pos.j + dx_
-        
-        if self.img[i][j] == 0 or i < 0 or i > self.ny or j < 0 or j > self.nx:
-          if len(vec) == 0:
-            vec = [0, 0]
-          vec[0] = vec[0] + dy_ if vec[0] + dy_ <= 1 and vec[0] + dy_ >= -1 else vec[0]
-          vec[1] = vec[1] + dx_ if vec[1] + dx_ <= 1 and vec[1] + dx_ >= -1 else vec[1]
-      
-    return vec
-  def sameRegion(self, vec, curr_vec):
-    # Boundary between two region
-    if vec[0] == 0 and vec[1] == 0:
-      return False
-    if curr_vec[0] == 0 and curr_vec[1] == 0:
-      return False
-    
-    # 0 degree angle
-    if vec[0] == curr_vec[0] and vec[1] == curr_vec[1]:
-      return True
-    
-    if vec[0] * curr_vec[0] + vec[1] * curr_vec[1] >= 0:
-      return True
-    
-    return False
-    
-  def getNeighborBoundary(self, pos, curr_vec):
-    ds = [-1, 0, 1, 0]
-    loc = []
-    curr_loc = Position(None, None)
-    for k in range(4):
-      dy = ds[(k + 3) % 4]
-      dx = ds[k]
-      
-      curr_loc.i = pos.i + dy
-      curr_loc.j = pos.j + dx
-      
-      if curr_loc.i < 0 or curr_loc.i >= self.ny: continue
-      if curr_loc.j < 0 or curr_loc.j >= self.nx: continue
-      vec = self.boundaryVector(curr_loc)
-      if len(vec) > 0:
-        if self.dp[curr_loc.i][curr_loc.j] == 1: continue
-        if self.sameRegion(vec, curr_vec):
-          loc.append(k)
-
-    return loc
-
-  def tranverse(self, stack):
-    while len(stack) > 0:
-      root_pos, root_vertex, direction = stack.pop()
-      curr_loc = Position(root_pos.i, root_pos.j)
-      self.update_pos[direction](curr_loc)
-      exist = False
-      
-      curr_vec = self.boundaryVector(curr_loc)
-      last_vec = curr_vec
-      while (curr_loc != self.end_pos[direction] and len(curr_vec) > 0):
-        if not self.sameRegion(curr_vec, last_vec):
-          dirs = self.getNeighborBoundary(curr_loc, last_vec)
-          for direction_ in dirs:
-            if direction_ != direction:
-              exist = True
-              break
-            
-          if exist:
-            # will be run atleast once
-            new_loc = Position(curr_loc.i, curr_loc.j)
-            new_vertex = Vertex(new_loc)
-            root_vertex.add_connection(new_vertex)
-            root_vertex = new_vertex
-
-          for direction_ in dirs:
-            if direction_ != direction:
-              stack.append([new_loc, new_vertex, direction_])
+    def __sweep(self, i, j, update_fnc, condition_fnc, end_pos):
+        while j != end_pos and condition_fnc(self.img[i][j]):
+            if self.__checkOtherRegion(i, j):
+                break
               
-          break
-          
-        self.dp[curr_loc.i][curr_loc.j] = 1
-        dirs = self.getNeighborBoundary(curr_loc, curr_vec)
+            # if self.dp[i][j] != 0:
+            #   break
+              
+            # self.__dp[i][j] = 1
+            j = update_fnc(j)
 
-        for direction_ in dirs:
-          if direction_ != direction:
-            exist = True
-            break
+        # j is either end pos 
+        # or j where self.img[i][j] != BG 
+        # or part of outer region
+        return j
+
+    def __findInterval(self, i, j):
+        # find the interval where pixels is BG
+        isBG = lambda v: v == self.__BG_PIXELS_INTENSITY
         
-        if exist:
-          # will be run atleast once
-          new_loc = Position(curr_loc.i, curr_loc.j)
-          new_vertex = Vertex(new_loc)
-          root_vertex.add_connection(new_vertex)
-          root_vertex = new_vertex
-
-          for direction_ in dirs:
-            if direction_ != direction:
-              stack.append([new_loc, new_vertex, direction_])
-
-          exist = False
-
-        last_vec = curr_vec
-        self.update_pos[direction](curr_loc)
-        curr_vec = self.boundaryVector(curr_loc)
-    
-  def __fill(self, img, loc, end_loc, direction):
-    ds = [-1, 0, 1, 0]
-    curr_loc = Position(loc.i, loc.j)
-    while curr_loc != end_loc:
-      for k in range(4):
-        i = curr_loc.i + ds[(3 + k) % 4]
-        j = curr_loc.j + ds[k]
-
-        if i < 0 or i >= self.ny: continue
-        if j < 0 or j >= self.nx: continue
-
-        if img[i][j] == 0:
-          img[i][j] = 255 
-          
-      self.update_pos[direction](curr_loc)
+        j_start = self.__sweep(i, j - 1, lambda x: x - 1, isBG, -1) + 1
+        j_end = self.__sweep(i, j + 1, lambda x: x + 1, isBG, self.nx) - 1
         
-  def findOutline(self):
-    outlines = []
-    stack = []
-    for i in range(self.ny):
-      for j in range(self.nx):
-        if self.dp[i][j] == 0:
-          start_pos = Position(i, j)
-          vec = self.boundaryVector(start_pos)
-          if len(vec) == 0: continue
-          if vec[0] == 0 and vec[1] == 0: continue
-          start_vertex = Vertex(start_pos)
-          dirs = self.getNeighborBoundary(start_pos, vec)
+        return (j_start, j_end)
 
-          # tranvrse new node
-          for direction in dirs:
-            stack.append([start_pos, start_vertex, direction])
+    def __fill(self, stack):
+        # count region total in stack
+        region_count = 0
 
-          self.tranverse(stack)
-          
-          outlines.append(start_vertex)
+        while len(stack) > 0:
+            # if region is connected to outer region, don't count
+            # if region is already visited, stop the search
+            data = stack.pop(0)
+            i_start, (j_start, j_end) = data
 
-    return outlines
-  
-  
-  def makeOutline(self, outlines, result = [], dict = None):
-    if len(result) == 0:
-      result = np.copy(self.img)
-      
-    if dict == None:
-      dict = {}
+            if self.__dp[i_start][j_start] != 0:
+                continue
 
-    for outline in outlines:
-      if dict.get((outline.key.i, outline.key.j), False) == True: continue
-      dict[(outline.key.i, outline.key.j)] = True
-      
-      for conn in outline.conn:
-        direction = Vertex.connectionDirection(outline.key, conn.key)
-        for k in range(4):
-          if direction[k] == 1:
-            self.__fill(result, outline.key, conn.key, k)
+            region_count += 1  # add region
+            i_start += 1
             
-      result = self.makeOutline(outline.conn, result)
-    
-    return result
+            # scan left then move the cursor down
+            while i_start < self.ny:
+                # find entry point
+                j_entry = -1
+                for j_ in range(max(j_start - 1, 0), min(j_end + 2, self.nx)):
+                  if self.img[i_start][j_] == self.__BG_PIXELS_INTENSITY:
+                    j_entry = j_
+                    break
+                  
+                if j_entry == -1:
+                  break
 
-def createTestImg(number):
-  f = open(abspath("src/test/testData/{}.txt".format(number)))
-  lines = map(lambda x: x.replace("\n", ""), f.readlines())
-  pixels = []
-  for line in lines:
-    pixels.append(list(map(lambda x: int(x), line.split(","))))
-  
-  pixels = np.array(pixels, dtype=c_ubyte)
-  return pixels
+                j_start_curr, j_end_curr = self.__findInterval(i_start, j_entry)
+                
+                # region is terminated
+                if j_start_curr > j_end + 1:
+                  break
+                
+                if j_end_curr < j_start - 1:
+                  break
+                
+                
+                # check if region is connected to outer region
+                # part of other region
+                if self.__checkOtherRegion(i_start, j_start_curr) or self.__checkOtherRegion(i_start, j_end_curr):
+                  region_count -= 1
+                  break
+                
+                # region is connected
+                for j_ in range(j_start_curr, j_end_curr + 1):
+                  self.__dp[i_start][j_] = 1
+                  
+                if j_end_curr == self.nx - 1 or j_start_curr == 0:
+                  region_count -= 1
+                  break
+                
+                
+                j_start = j_start_curr
+                j_end = j_end_curr
+                
+                  
+                i_start += 1
+                
+        return region_count
+
+    def __existUpper(self, i, j):
+      for k in range(-1, 2):
+        if j + k >= self.nx or j + k < 0:
+          continue
+        
+        if self.img[i - 1][j + k] == self.__BG_PIXELS_INTENSITY:
+          return True
+      return False
+    
+    def loop_count(self):
+        stack = []
+        for i in range(self.ny):
+            j = 0
+            while j < self.nx:
+                self.__dp[i][j] = -1
+                if self.img[i][j] != self.__BG_PIXELS_INTENSITY:
+                    jr = j  # j start region
+                    if i - 1 < 0:
+                        break
+                    # start of a new region 
+                    isExistUpper  = False
+                    while jr < self.nx and self.img[i][jr] != self.__BG_PIXELS_INTENSITY:
+                        jr += 1
+
+                    jsr = jr  # j stop region
+                    while jsr < self.nx and self.img[i][jsr] == self.__BG_PIXELS_INTENSITY:
+                        if self.__existUpper(i, jsr):
+                          isExistUpper = True
+                        self.__dp
+                        jsr += 1
+                        
+                    if jsr == self.nx:
+                        break
+
+                    # region not connected to outer region
+                    # and not connected to other region
+                    if not isExistUpper:
+                      stack.append([i, (jr, jsr - 1)])
+                    
+                    if len(stack) > 1 and isExistUpper and stack[-1][0] == i:
+                      stack.append([i, (jr, jsr)])
+                    
+                    j = jsr
+
+                else:
+                    j += 1
+
+        return self.__fill(stack)
+      
+
 
 
       
