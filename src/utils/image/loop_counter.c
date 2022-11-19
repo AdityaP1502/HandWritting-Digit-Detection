b353d228ae4b1bd388a9153d6dda31a7852503d2
AdityaP1502 <i.m.aditya015@gmail.com>
2022-11-20 03:26:48 +0700

    implement new loop count algorithm

--- src/utils/image/loop_counter.c
+++ src/utils/image/loop_counter.c
@@ -5,389 +5,261 @@
 #include "../../header/stack.h"
 #include "../../header/loop_counter.h"
 
-typedef struct Data_
-{
-    POS pos;
-    int direction;
-} Data;
-
-typedef Data *DATA;
-
-static void update_pos_left(POS pos)
-{
-    pos->j--;
+static int max(int a, int b) {
+  // if a - b >= 0 then a - b >> 31 = 0, therefore yield a
+  // if a - b << 0 then a - b >> 31 = -1, therefore yield b
+  return a - ((a - b) & (a - b) >> (sizeof(a) * 8 - 1));
 }
 
-static void update_pos_dowm(POS pos)
-{
-    pos->i--;
-}
-
-static void update_pos_right(POS pos)
-{
-    pos->j++;
-}
-
-static void update_pos_up(POS pos)
-{
-    pos->i++;
-}
-
-static updatePos update_fnc[4] = {
-    update_pos_left,
-    update_pos_dowm,
-    update_pos_right,
-    update_pos_up,
-};
-
-static int equal_end_pos(POS a, POS end_pos)
-{
-    if (end_pos->i == -2)
-    {
-        return end_pos->j == a->j;
-    }
-
-    if (end_pos->j == -2)
-    {
-        return end_pos->i == a->i;
-    }
-
-    return (end_pos->i == a->i && end_pos->j == a->j);
+static int min(int a, int b) {
+  // if a < b, then -a > -b, therefore min(a, b) = max(-a, -b)
+  return -1 * max(-a, -b);
 }
 
 loopCounter *loop_counter_init(IMAGE img)
 {
+    // initialize memory
     loopCounter *counter = malloc(sizeof(loopCounter));
-    counter->img = img;
-    uint8_t *dp = calloc(img->nx * img->ny, sizeof(uint8_t));
+    checkmem(counter);
+
     STACK s = stack_init();
-    POS *end_pos = malloc(4 * sizeof(POS));
 
-    // initialize end position
-    // end pos left
-    POS end_pos_left = malloc(sizeof(Position));
-    end_pos_left->i = -2;
-    end_pos_left->j = -1;
-    end_pos[0] = end_pos_left;
-
-    // end pos left
-    POS end_pos_down = malloc(sizeof(Position));
-    end_pos_down->i = -1;
-    end_pos_down->j = -2;
-    end_pos[1] = end_pos_down;
-
-    // end pos right
-    POS end_pos_right = malloc(sizeof(Position));
-    end_pos_right->i = -2;
-    end_pos_right->j = img->nx;
-    end_pos[2] = end_pos_right;
-
-    // end pos up
-    POS end_pos_up = malloc(sizeof(Position));
-    end_pos_up->i = img->ny;
-    end_pos_up->j = -2;
-    end_pos[3] = end_pos_up;
+    uint8_t* dp = calloc((img->nx * img->ny), sizeof(uint8_t));
+    checkmem(dp);
 
     counter->img = img;
-    counter->update_fnc = update_fnc;
-    counter->end_pos = end_pos;
-    counter->dp = dp;
     counter->s = s; // stack
+    counter->dp = dp;
 
     return counter;
 }
 
-static int sameRegion(int* vec_a, int* vec_b) {
-    // boundary
-    if (vec_a[0] == 0 && vec_a[1] == 0) return 0;
+static int is_BG(uint8_t val) 
+{
+    return val == BG_PIXELS_INTENSITY;
+}
 
-    // boundary
-    if (vec_b[0] == 0 && vec_b[1] == 0) return 0;
+static int update_j_left(int j) 
+{
+    return j - 1;
+}
 
-    // 0 degree angle
-    if ((vec_a[0] == vec_b[0]) && (vec_a[1] == vec_b[1])) return 1;
+static int update_j_right(int j)
+{
+    return j + 1;
+}
 
-    // 45 and 90 degree angle
-    if (vec_a[0] * vec_b[0] + vec_a[1] * vec_b[1] >= 0) return 1;
+static int is_part_of_other_region(loopCounter* counter, int i, int j) 
+{
+    return image_read_serial(
+        counter->dp, counter->img->nx, i, j) != 0;
+}
+
+static int sweep(loopCounter* counter, int i, int j, update_fnc f, condition_fnc c, int end_pos) {
+    uint8_t pix_intensity = image_read_serial(counter->img->img, counter->img->nx, i, j);
+    while (j != end_pos && c(pix_intensity)) 
+    {
+        if (is_part_of_other_region(counter, i, j)) break;
+        j = f(j);
+        pix_intensity = image_read_serial(counter->dp, counter->img->nx, i, j);
+    }
+
+    // j is either end pos
+    // or j is not BG
+    // or part of other region
+    return j;
+}
+
+static void find_interval(int* dst, loopCounter* counter, int i, int j) 
+{
+    // find interval where pixels is BG
+    int j_start = sweep(counter, i, j - 1, update_j_left, is_BG, -1) + 1;
+    int j_end = sweep(counter, i, j + 1, update_j_right, is_BG, counter->img->nx) - 1;
+
+    dst[0] = j_start;
+    dst[1] = j_end;
+
+}
+
+static int fill(loopCounter* counter) {
+    int i_start, j_start, j_end, j_entry, j_start_curr, j_end_curr;
+    int region_count;
+    int s, e;
+    uint8_t pix;
+    int interval[] = {0, 0};
+
+    region_count = 0;
+    while (!stack_is_empty(counter->s))
+    {
+        DATA data = stack_pop(counter->s);
+        i_start = data->i;
+        j_start = data->j_start;
+        j_end = data->j_end;
+        free(data);
+
+        if (is_part_of_other_region(counter, i_start, j_start)) continue;
+        region_count++;
+        i_start++;
+
+        while (i_start < counter->img->ny) {
+            // find entry point
+            j_entry = -1;
+            s = max(0, j_start - 1);
+            e = min(j_end + 2, counter->img->nx);
+
+            for (int j = s; j < e; j++) 
+            {
+                pix = image_read_serial(counter->img->img, counter->img->nx, i_start, j);
+                if (pix == BG_PIXELS_INTENSITY) {
+                    j_entry = j;
+                    break;
+                }
+            }
+
+            // region is terminated here
+            if (j_entry == -1) break;
+
+            find_interval(interval, counter, i_start, j_entry);
+            // unpack value
+            j_start_curr = interval[0];
+            j_end_curr = interval[1];
+
+            // if (j_start_curr > j_end + 1) break;
+            // if (j_end_curr < j_start - 1) break;
+
+            // check if region is connected to other region
+            if (is_part_of_other_region(counter, i_start, j_start_curr)) 
+            {
+                region_count--;
+                break;
+            }
+
+            if (is_part_of_other_region(counter, i_start, j_end_curr)) {
+                region_count--;
+                break;;
+            }
+
+            // region is connected
+            // update dp 
+            for (int j = j_start_curr; j < j_end_curr + 1; j++) 
+            {
+                image_write_serial(
+                    counter->dp, counter->img->nx, 
+                    i_start, j, 1
+                );
+            }
+
+            if (j_end_curr == counter->img->nx - 1 || j_start == 0) {
+                region_count--;
+                break;
+            }
+
+            j_start = j_start_curr;
+            j_end = j_end_curr;
+
+            i_start++;
+        }
+
+    }
+
+    return region_count;
+}
+
+static int is_exist_upper(loopCounter* counter, int i, int j) 
+{
+    int pix;
+    for (int k = -1; k < 2; k++) 
+    {
+        if (j + k < 0 || j + k >= counter->img->nx) continue;
+        pix = image_read_serial(counter->img->img, counter->img->nx, i - 1, j + k);
+        if (pix == BG_PIXELS_INTENSITY) return 1;
+    }
 
     return 0;
 }
 
-static int isBoundary(int* vec) {
-    return !(vec[0] == 2 && vec[1] == 2);
-}
-
-
-static void boundaryVector(loopCounter *counter, POS pos, int* vec)
+static void init_stack(loopCounter* counter) 
 {
-    if (!vec) die("NullPointerException: Accessing a null pointer");
-    vec[0] = 2; vec[1] = 2; // initialize vector
-    if (pos->i < 0 || pos->i >= counter->img->ny)
-        return;
-    if (pos->j < 0 || pos->j >= counter->img->nx)
-        return;
-    if (image_read_serial(counter->img->img, counter->img->nx, pos->i, pos->j) == BG_PIXELS_INTENSITY)
-        return;
-
-    int a, b;
-    int pos_i, pos_j;
-    uint8_t pix_intensity;
-    int dx[3] = {-1, 0, 1};
-    int dy[3] = {-1, 0, 1};
-
-
-    // int* vec = NULL;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
+    int i, j, pix;
+    int jr, jsr;
+    int exist_upper;
+    DATA u;
+    for (i = 1; i < counter->img->ny; i++) {
+        j = 0;
+        while (j < counter->img->nx) 
         {
-            pos_i = pos->i + dy[i];
-            pos_j = pos->j + dx[j];
-
-            pix_intensity = image_read_serial(counter->img->img, counter->img->nx, pos_i, pos_j);
-            if (pix_intensity == BG_PIXELS_INTENSITY || pos_i < 0 || pos_i == counter->img->ny || pos_j < 0 || pos_j == counter->img->nx) {
-                // if (!vec) {
-                //     vec = calloc(2, sizeof(int));
-                // }
-                if (!isBoundary(vec)) {
-                    vec[0] = 0;
-                    vec[1] = 0;
-                }
-                a = vec[0] + dy[i];
-                b = vec[1] + dx[j];
-                vec[0] = a <= 1 && a >= -1 ? a : vec[0];
-                vec[1] = b <= 1 && b >= -1 ? b : vec[1];
-            }
-        }
-    }
-}
-
-
-static int *getNeighborBoundary(loopCounter *counter, POS pos, int* curr_vec)
-{
-    int dy, dx, c;
-    c = 0;
-    int ds[4] = {-1, 0, 1, 0};
-
-    int *loc = malloc(4 * sizeof(int));
-    loc[0] = -1;
-    loc[1] = -1;
-    loc[2] = -1;
-    loc[3] = -1;
-
-    POS curr_loc = malloc(sizeof(Position));
-    curr_loc->i = pos->i;
-    curr_loc->j = pos->j;
-    int* vec = malloc(2 * sizeof(int));
-
-    for (int k = 0; k < 4; k++)
-    {
-        dy = ds[(k + 3) % 4];
-        dx = ds[k];
-
-        curr_loc->i = pos->i + dy;
-        curr_loc->j = pos->j + dx;
-
-        if (curr_loc->i < 0 || curr_loc->i == counter->img->ny)
-            continue;
-        if (curr_loc->j < 0 || curr_loc->j == counter->img->nx)
-            continue;
-
-        boundaryVector(counter, curr_loc, vec);
-        if (isBoundary(vec))
-        {
-            if (image_read_serial(counter->dp, counter->img->nx, curr_loc->i, curr_loc->j)) {
-                continue;
-            }
-            if (!sameRegion(vec, curr_vec)) {
-                continue;
-            }
-            loc[c] = k;
-            c++;
-        }
-    }
-
-    free(vec);
-    free(curr_loc);
-    return loc;
-}
-
-static int find_new_node(loopCounter* counter, POS curr_loc, int* curr_vec, int direction) {
-    int *directions;
-    int i, count;
-    int exist = 0;
-    directions = getNeighborBoundary(counter, curr_loc, curr_vec);
-
-    i = 0;
-    count = 1;
-
-    while (directions[i] != -1)
-    {
-        if (directions[i] != direction)
-        {
-            exist = 1;
-            break;
-        }
-        i++;
-    }
-
-    if (exist)
-    {
-        POS new_loc = malloc(sizeof(Position));
-        new_loc->i = curr_loc->i;
-        new_loc->j = curr_loc->j;
-        i = 0;
-        while (directions[i] != -1)
-        {   
-            if (directions[i] != direction)
+            image_write_serial(counter->dp, counter->img->nx, i, j, 1);
+            pix = image_read_serial(counter->img->img, counter->img->nx, i, j);
+            if (pix != BG_PIXELS_INTENSITY) 
             {
-                DATA new_data = malloc(sizeof(Data));
-                new_data->pos = new_loc;
-                new_data->direction = directions[i];
-                stack_push(counter->s, new_data);
-                count++;
+                jr = j; // start region
+                exist_upper = 0;
+                while (jr < counter->img->nx && pix != BG_PIXELS_INTENSITY) 
+                {
+                    jr++;
+                    pix = image_read_serial(counter->img->img, counter->img->nx, i, jr);
+                }
+
+                jsr = jr;
+                while (jsr < counter->img->nx && pix == BG_PIXELS_INTENSITY) 
+                {
+                    if (is_exist_upper(counter, i, jsr))  exist_upper = 1;
+                    jsr++;
+                    pix = image_read_serial(counter->img->img, counter->img->nx, i, jsr);
+                }
+
+                if (jsr == counter->img->nx) break;
+
+                // not part of any other region
+                if (!exist_upper) {
+                    DATA data = malloc(sizeof(Data));
+                    data->i = i;
+                    data->j_start = jr;
+                    data->j_end = jsr - 1;
+                    stack_push(counter->s, (void*) data);
+                }
+
+                // check if part of upper region 
+                // but not connected
+                u = stack_peek(counter->s);
+                if (exist_upper && !stack_is_empty(counter->s) && counter->s->head->next && u->i == i) 
+                {
+                    DATA data = malloc(sizeof(Data));
+                    data->i = i;
+                    data->j_start = jr;
+                    data->j_end = jsr - 1;
+                    stack_push(counter->s, (void*) data);
+                }
+
+                j = jsr;
+            } else 
+            {
+                j += 1;
             }
-            i++;
         }
     }
-
-
-    free(directions);
-    return count;
 }
 
-static void tranverse(loopCounter *counter)
-{
-    int count;
-    DATA data;
-    POS end_pos;
-    POS root_pos;
-    int *curr_vec = malloc(2 * sizeof(int));
-    int *last_vec = malloc(2 * sizeof(int));
-    POS curr_loc = malloc(sizeof(Position)); // temp variables
-    while (!stack_is_empty(counter->s))
-    {
-        data = stack_pop(counter->s);
-
-        root_pos = data->pos;
-        int direction = data->direction;
-
-        end_pos = counter->end_pos[direction];
-        curr_loc->i = root_pos->i;
-        curr_loc->j = root_pos->j;
-
-        // update position
-        counter->update_fnc[direction](curr_loc);
-
-        // initialize vector
-        boundaryVector(counter, curr_loc, curr_vec);
-        last_vec[0] = curr_vec[0];
-        last_vec[1] = curr_vec[1];
-
-        while (!equal_end_pos(curr_loc, end_pos) && isBoundary(curr_vec))
-        {
-            if (image_read_serial(counter->dp, counter->img->nx, curr_loc->i, curr_loc->j)) break;
-            if (!sameRegion(curr_vec, last_vec)) {
-                count = find_new_node(counter, curr_loc, last_vec, direction);
-                break;
-            }   
-
-            count = find_new_node(counter, curr_loc, curr_vec, direction);
-            image_write_serial(counter->dp, counter->img->nx, curr_loc->i, curr_loc->j, count);
-            
-            last_vec[0] = curr_vec[0];
-            last_vec[1] = curr_vec[1];
-
-            counter->update_fnc[direction](curr_loc);
-            // update curr_vec
-            boundaryVector(counter, curr_loc, curr_vec);
-        }
-
-        // delete unused memory
-        count = image_read_serial(counter->dp, counter->img->nx, root_pos->i, root_pos->j);
-        if (count != 2) 
-        { 
-            image_write_serial(counter->dp, counter->img->nx, root_pos->i, root_pos->j, count - 1);
-        } else {
-            free(data->pos);
-        }
-        // free(data->pos);
-        free(data);
-    }
-
-    free(curr_loc);
-    free(curr_vec);
-    free(last_vec);
-}
 int loop_count(loopCounter *counter)
 {
-    int c;
-    int outline_count = 0;
-    int* vec = malloc(2 * sizeof(int));
-    for (int i = 0; i < counter->img->ny; i++)
-    {
-        for (int j = 0; j < counter->img->nx; j++)
-        {
-            if (image_read_serial(counter->img->img, counter->img->nx, i, j) != BG_PIXELS_INTENSITY)
-            {
-                if (!image_read_serial(counter->dp, counter->img->nx, i, j))
-                {
-                    POS start_pos = malloc(sizeof(Position));
-                    start_pos->i = i;
-                    start_pos->j = j;
-                    boundaryVector(counter, start_pos, vec);
-                    // printf("i=%d,j=%d\n", i, j);
-                    // printf("x=%d,y=%d\n", vec[0], vec[1]);
-                    if (vec[0] == 0 && vec[1] == 0) continue;
-                    if (isBoundary(vec))
-                    {
-                        int *dirs = getNeighborBoundary(counter, start_pos, vec);
-                        c = 0;
-                        outline_count++;
-                        // printf("i=%d,j=%d\n", i, j);
-                        while (dirs[c] != -1)
-                        {
-                            DATA new_data = malloc(sizeof(Data));
-                            new_data->pos = start_pos;
-                            new_data->direction = dirs[c];
-                            stack_push(counter->s, new_data);
-                            c++;
-                        }
-                        // update dpe
-                        image_write_serial(counter->dp, counter->img->nx, start_pos->i, start_pos->j, c + 1);
-                        free(dirs);
-                        tranverse(counter);
-                    }   
-                    else 
-                    {
-                        free(start_pos);
-                    }
-                }
-            }
-        }
-    }
+    init_stack(counter);
+    int cnt = fill(counter);
 
-    // free all contents in counter
+    // clear memory before exiting
     free(counter->dp);
-    free(vec);
-    for (int i = 0 ; i < 4; i++) {
-        free(counter->end_pos[i]);
-    }
-
-    free(counter->end_pos);
     stack_destroy(counter->s);
-    // freee counter
     free(counter);
 
-    return outline_count - 1;
+    return cnt;
 }
+
 int python_loop_count(uint8_t *img, int nx, int ny)
 {
-    IMAGE img_data = python_read_image(img, nx, ny);
-    // IMAGE img_data = malloc(sizeof(Image));
-    // img_data->img = img;
-    // img_data->nx = nx;
-    // img_data->ny = ny;
+    // IMAGE img_data = python_read_image(img, nx, ny);
+    IMAGE img_data = malloc(sizeof(Image));
+    img_data->img = img;
+    img_data->nx = nx;
+    img_data->ny = ny;
     
     loopCounter *counter = loop_counter_init(img_data);
     int n = loop_count(counter);
