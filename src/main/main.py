06f2b7278d6a91d5e3cfd3022b3fc5dba7eded62
AdityaP1502 <i.m.aditya015@gmail.com>
2022-11-27 14:24:08 +0700

    Update main.py

--- src/main/main.py
+++ src/main/main.py
@@ -1,4 +1,5 @@
 import os.path
+import os
 assert os.path.abspath(".").split("/")[-1] != "main", "run main.py from root folder"
 
 import getopt
@@ -8,18 +9,21 @@ from time import time
 
 from helper import *
 from image import *
+from load import load_models
 from loadingAnimation import Loading, Color
 from c_interface import serializeArray_c, toMatrix, bbox_pipeline, thresh_pipeline
 
 
 IMG_DIRPATH = IMG_DIRPATH_DEFAULT
 FILENAME = ""
+BATCH_MODE = False
 
 def showHelp():
   print("""Accepted Arguments:
           -h : show help screen
-          -f : filename.
-          -d : enable debug mode. Will save detected shape. 
+          -f : filename. (Use this if -b is not specified)
+          -d : enable debug mode. will save detected shape.
+          -b :  enable batch mode. Will load picture from path given in --img-path
           --img-path=<image_path> : Frame input path. Use absolute path. Use this if image not in default location.
           """)
 
@@ -31,6 +35,9 @@ def showInfo():
     
     if DEBUG_MODE:
         print(Color.print_colored("USING DEBUG MODE", utils=["bold"]))
+        
+    if BATCH_MODE:
+        print(Color.print_colored("USING BATCH MODE", utils=["bold"]))
 
 def pipeline(data):
     img, partition, j, idx = data
@@ -73,7 +80,6 @@ def process(result, mat, s):
     # do prediction
     total_shapes = len(numbers)
     
-    from load import load_models
     model = load_models()
     prediction_ = model.predict(numbers.reshape(total_shapes, 28, 28, 1))
     prediction = list(map(lambda x: np.argmax(x), prediction_))
@@ -82,38 +88,9 @@ def process(result, mat, s):
         result.append(prediction[i])
         
 
-if __name__ == "__main__":
-    try:
-        opts, args = getopt.getopt(argv[1:], shortopts="hdf:", longopts=["img-path="])
-    except getopt.GetoptError as err:
-        print(err)
-        print("Error : Invalid Argument")
-        showHelp()
-        exit(1)
-
-    for opt, arg in opts:
-        if (opt == "-h"):
-            showHelp()
-            exit(0)
-
-        if (opt == "-f"):
-            FILENAME = arg
-        
-        if (opt == "-d"):
-            DEBUG_MODE = True
-            os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0' 
-            
-        if (opt == "--img-path"):
-            IMG_DIRPATH = arg
-
-    showInfo()
-    assert FILENAME != "", "FILENAME must be specified. Run this with -f options"
-
-    IMG_FILEPATH = os.path.join(IMG_DIRPATH, FILENAME)
-    assert os.path.isfile(IMG_FILEPATH), "File not found in path specified: {}".format(IMG_FILEPATH)
-    start_time = time()
+def routine(img_path):
     # load image
-    img2 = Images(IMG_FILEPATH)
+    img2 = Images(img_path)
     img2.toGrayscale()
 
     # removce shadow
@@ -143,16 +120,72 @@ if __name__ == "__main__":
     # do detection
     result = []
     Loading.loading(process_fnc=process, args=(result, mat, s, ))
+    print()
+    
+    return result
+
+if __name__ == "__main__":
+    try:
+        opts, args = getopt.getopt(argv[1:], shortopts="hdf:", longopts=["img-path="])
+    except getopt.GetoptError as err:
+        print(err)
+        print("Error : Invalid Argument")
+        showHelp()
+        exit(1)
+
+    for opt, arg in opts:
+        if (opt == "-h"):
+            showHelp()
+            exit(0)
+
+        if (opt == "-f"):
+            FILENAME = arg
+        
+        if (opt == "-d"):
+            DEBUG_MODE = True
+            os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0' 
+            
+        if (opt == "--img-path"):
+            IMG_DIRPATH = arg
+            
+        if (opt == "-b"):
+            BATCH_MODE = True
+
+    showInfo()
+    start_time = time()
+    
+    if not BATCH_MODE:
+        assert FILENAME != "", "FILENAME must be specified. Run this with -f options"
+        img_path = os.path.join(IMG_DIRPATH, FILENAME)
+        assert os.path.isfile(img_path), "File not found in path specified: {}".format(img_path)
+        result = routine(img_path)
+        print("Here is the result:")
+        for i in range(len(result)):
+            print(result[i], end = " ")
+        print()
+            
+    else:
+        assert os.path.isdir(IMG_DIRPATH), "Directory does not exist"
+        dir_list = os.listdir(IMG_DIRPATH)
+        extension = map(lambda x: x.split(".")[0], dir_list)
+        img_list  = [file for (i, file) in enumerate(dir_list) if extension[i] == 'jpg' or extension[i] == 'png']
+        with Pool(cpu_count()) as pool:
+            results = pool.map(routine, img_list)
+        
+        print("The results is:")
+        for i, img_name in enumerate(img_list):
+            prompt = Color.print_colored("{}: ".format(img_name), color_fg=[0, 255, 255], utils=["bold"])
+            print(prompt, end = "")
+            for pred in results[i]:
+                print(pred, end=" ")
+            print()
+        
+        
     end_time = time()
     elapsed_time = (end_time - start_time)
     elapsed_time_h = elapsed_time // 3600
     elapsed_time_m = (elapsed_time % 3600) // 60
     elapsed_time_s = (elapsed_time % 3600) % 60
-    print()
+    
     prompt = Color.print_colored("Took about {:.2f} hours {:.2f} minutes {:.2f} seconds".format(elapsed_time_h, elapsed_time_m, elapsed_time_s), color_fg=[0, 255, 255], utils=["bold"])
     print(prompt)
-    print("Here is the result:")
-    print(len(result))
-    for i in range(len(result)):
-        print(result[i], end = " ")
-    print()
