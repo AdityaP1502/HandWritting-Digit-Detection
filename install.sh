eb46025e094cc197b69641ba3bd82994511ff1ed
AdityaP1502 <i.m.aditya015@gmail.com>
2022-12-02 21:08:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-MODEL_NAME="model.tar"
-MODEL_ID=1-VnALakyh-OysIe0lSDW8fIlQg0XKnxz
+MODEL_NAME="model.tar.gz"
+MODEL_ID=1_A4C8A0qJ8WKMdtvHI5J-Hqc_6PXRmH4
 FILE=$PWD/$MODEL_NAME
 echo $FILE
 
@@ -10,8 +10,8 @@ chmod +x scripts/build.sh && chmod +x scripts/buildTest.sh
 
 echo "Checking model files"
 if [ -f "$FILE" ]; then
-    sha256sum -c MODEL_SHA256
-    if [  $?  ]; then
+    sha256sum -c MODEL_SHA256SUMS
+    if [ ! $? -eq 0  ]; then
         echo "File is corrupted, redownload model files"
         rm -rf $MODEL_NAME
         wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$MODEL_ID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$MODEL_ID" -O $MODEL_NAME && rm -rf /tmp/cookies.txt
@@ -20,13 +20,15 @@ if [ -f "$FILE" ]; then
 else 
     echo "$FILE does not exist."
     echo "Downloading model files"
-    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$MODEL_ID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$MODEL_ID" -O $MODEL_NAME && rm -rf /tmp/cookies.txt
+    wget --load-cookies $PWD/tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies $PWD/tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$MODEL_ID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$MODEL_ID" -O $MODEL_NAME && rm -rf $PWD/tmp/cookies.txt
+    sha256sum -c MODEL_SHA256SUMS
+    if [ $? ]; then
+        exit 1
+    fi
 fi
-tar -xvf $MODEL_NAME  && rm -rf $MODEL_NAME
-
-if [ ! $? ]; then
+tar -xvf $MODEL_NAME && rm -rf $MODEL_NAME
+if [ $? -eq 0 ]; then
     echo "Installation Succesful"
 else
     echo "Installation failed"
 fi
-
